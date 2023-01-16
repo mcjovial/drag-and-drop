bfb10e2fc8cac8e2b7650920f6c7847cbc0927a1
Emmanuel Elias <emmanuelelias455@gmail.com>
2023-01-16 13:50:24 +0100

    Customise the Appearance of an App during a Drag using react-beautiful-dnd snapshot Values

--- src/Column.jsx
+++ src/Column.jsx
@@ -3,6 +3,13 @@ import { Droppable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 import Task from './Task';
 
+// // // example snapshot
+// // Draggable
+// const droppableSnapshot = {
+//   isDraggingOver: true,
+//   draggingOverWith: 'task-1',
+// };
+
 const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
@@ -13,6 +20,7 @@ const Title = styled.h3`
 `;
 const TaskList = styled.div`
   padding: 8px;
+  background-color: ${props => (props.isDraggingOver? 'skyblue': 'white')};
 `;
 
 const Column = ({ column, tasks }) => {
@@ -20,8 +28,8 @@ const Column = ({ column, tasks }) => {
     <Container>
       <Title>{column.title}</Title>
       <Droppable droppableId={column.id}>
-        {(provided) => (
-          <TaskList ref={provided.innerRef} {...provided.droppableProps}>
+        {(provided, snapshot) => (
+          <TaskList ref={provided.innerRef} {...provided.droppableProps} isDraggingOver={snapshot.isDraggingOver}>
             {tasks.map((task, index) => (
               <Task key={task.id} task={task} index={index} />
             ))}
--- src/Task.jsx
+++ src/Task.jsx
@@ -2,22 +2,30 @@ import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
 import styled from 'styled-components';
 
+// // // example snapshot
+// // Draggable
+// const draggableSnapshot = {
+//   isDragging: true,
+//   draggingOver: 'column-1',
+// };
+
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
-  background-color: white;
+  background-color: ${props => (props.isDragging? 'lightgreen': 'white')};
 `;
 
 const Task = ({ task, index }) => {
   return (
     <Draggable draggableId={task.id} index={index}>
-      {(provided) => (
+      {(provided, snapshot) => (
         <Container
           {...provided.draggableProps}
           {...provided.dragHandleProps}
           ref={provided.innerRef}
+          isDragging={snapshot.isDragging}
         >
           {task.content}
         </Container>
